2d2272257bd369a1718ba59304fb456664bd76d8
Albert Huynh <ahuynh@fanatics.com>
2019-01-18 19:29:15 -0800

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'hyphy-ipsum',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ahuynh',
+      host: 'ec2-54-215-240-23.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/hyphy-ipsum.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/alberthuynh91/hyphy-ipsum.git',
+      path: '/Users/ahuynh/Desktop/hyphy-ipsum',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
